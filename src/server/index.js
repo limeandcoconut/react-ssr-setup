b2f48ebb2c977ac5234f4bacbd480c37cf771eb5
Jacob Smith <messagethesmith@gmail.com>
2019-03-13 11:37:53 -0700

    re-lint index server

--- src/server/index.js
+++ src/server/index.js
@@ -1,46 +1,46 @@
 // import React from 'react';
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import chalk from 'chalk';
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import chalk from 'chalk'
 // import manifestHelpers from 'express-manifest-helpers';
-import manifestHelpers from './middleware/manifest-helpers';
-import bodyParser from 'body-parser';
-import { configureStore } from '../shared/store';
-import serverRender from './render';
-import paths from '../../config/paths';
+import manifestHelpers from './middleware/manifest-helpers'
+import bodyParser from 'body-parser'
+import {configureStore} from '../shared/store'
+import serverRender from './render'
+import paths from '../../config/paths'
 
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
+const app = express()
 
 // Use Nginx or Apache to serve static assets in production or remove the if() around the following
 // lines to use the express.static middleware to serve assets for production (not recommended!)
 if (process.env.NODE_ENV === 'development') {
-    app.use(paths.publicPath, express.static(path.join(paths.clientBuild, paths.publicPath)));
+    app.use(paths.publicPath, express.static(path.join(paths.clientBuild, paths.publicPath)))
     app.use('/favicon.ico', (req, res) => {
-        res.send('');
-    });
+        res.send('')
+    })
 }
 
-app.use(cors());
+app.use(cors())
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    req.store = configureStore();
-    return next();
-});
+    req.store = configureStore()
+    return next()
+})
 
-const manifestPath = path.join(paths.clientBuild, paths.publicPath);
+const manifestPath = path.join(paths.clientBuild, paths.publicPath)
 
 app.use(
     manifestHelpers({
         manifestPath: `${manifestPath}/manifest.json`,
     })
-);
+)
 
-app.use(serverRender());
+app.use(serverRender())
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
@@ -51,29 +51,29 @@ app.use((err, req, res, next) => {
             // print a nicer stack trace by splitting line breaks and making them array items
             process.env.NODE_ENV === 'development' &&
             (err.stack || '')
-                .split('\n')
-                .map((line) => line.trim())
-                .map((line) => line.split(path.sep).join('/'))
-                .map((line) =>
-                    line.replace(
-                        process
-                            .cwd()
-                            .split(path.sep)
-                            .join('/'),
-                        '.'
-                    )
-                ),
-    });
-});
+            .split('\n')
+            .map((line) => line.trim())
+            .map((line) => line.split(path.sep).join('/'))
+            .map((line) =>
+                line.replace(
+                    process
+                    .cwd()
+                    .split(path.sep)
+                    .join('/'),
+                    '.'
+                )
+            ),
+    })
+})
 
 app.listen(process.env.PORT || 8500, () => {
     console.log(
         `[${new Date().toISOString()}]`,
         chalk.blue(
             `App is running: 🌎 ${process.env.HOST || 'http://localhost'}:${process.env.PORT ||
-                8500}`
+            8500}`
         )
-    );
-});
+    )
+})
 
-export default app;
+export default app
